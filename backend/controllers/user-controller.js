708030f84f9e77894c08aef352ba852450eb0576
BumbleGoose <i.r.r.laurie@gmail.com>
2022-09-06 15:44:13 +0100

    add get all users endpoint and associated test suite

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,7 +1,10 @@
 const { fetchAllUsers } = require(`${__dirname}/../models/user-models.js`);
 
 exports.getAllUsers = (req, res, next) => {
-  fetchAllUsers()
+  const { sort_by: sortBy } = req.query;
+  const { order } = req.query;
+
+  fetchAllUsers(sortBy, order)
     .then((allUsers) => {
       res.status(200).send({ users: allUsers });
     })
