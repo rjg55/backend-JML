fa782187d888e93a7651f42c0f3e4e6d7c3033ec
GWorsnop <georgeworsnop@gmail.com>
2022-09-07 15:51:58 +0100

    Add error handling and tests to events-messages

--- backend/controllers/event-message-controller.js
+++ backend/controllers/event-message-controller.js
@@ -1,7 +1,7 @@
 const {
   fetchAllEventMessages,
-  fetchEventMessageById,
-  insertMessage
+  insertMessage,
+  removeMessage,
 } = require(`../models/event-message-models`);
 
 // GET all event messages by event Id
@@ -23,7 +23,19 @@ exports.addMessage = (req, res, next) => {
       res.status(201).send({ message });
     })
     .catch((err) => {
+      next(err.errors.message.properties);
+    });
+};
+
+exports.deleteMessage = (req, res, next) => {
+  const { id } = req.params;
+  console.log(id);
+  removeMessage(id)
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch((err) => {
       console.log(err);
-      next();
+      next(err);
     });
 };
--- backend/routes/event-message-routes.js
+++ backend/routes/event-message-routes.js
@@ -1,15 +1,17 @@
-const eventMessageRouter = require('express').Router();
+const eventMessageRouter = require("express").Router();
 
 // REQUIRE IN CONTROLLER FUNCTIONS HERE
 const {
   getAllMessagesByEventId,
-  addMessage
-} = require('../controllers/event-message-controller');
+  addMessage,
+  deleteMessage,
+} = require("../controllers/event-message-controller");
 
-// Put endpoints here
 eventMessageRouter
-  .route('/events/:event_id')
+  .route("/events/:event_id")
   .get(getAllMessagesByEventId)
   .post(addMessage);
+
+eventMessageRouter.route("/:id").delete(deleteMessage);
 
 module.exports = eventMessageRouter;
