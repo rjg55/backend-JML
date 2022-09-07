f454d3ec90b057435659bf44245824b836b788e6
Damian Martin <dmartinr2d2@gmail.com>
2022-09-07 14:28:34 +0100

    add tests for messages and controller functions

--- backend/controllers/event-message-controller.js
+++ backend/controllers/event-message-controller.js
@@ -1,9 +1,29 @@
 const {
   fetchAllEventMessages,
-} = require(`${__dirname}/../models/event-message-models.js`);
+  fetchEventMessageById,
+  insertMessage
+} = require(`../models/event-message-models`);
 
-exports.getAllEventMessages = (req, res, next) => {
-  fetchAllEventMessages()
-    .then(() => {})
+// GET all event messages by event Id
+exports.getAllMessagesByEventId = (req, res, next) => {
+  const { event_id } = req.params;
+  fetchAllEventMessages(event_id)
+    .then((messages) => {
+      res.status(200).send(messages);
+    })
     .catch(next);
+};
+
+//POST comment
+exports.addMessage = (req, res, next) => {
+  const { event_id } = req.params;
+  const newMessage = req.body;
+  insertMessage(event_id, newMessage)
+    .then((message) => {
+      res.status(201).send(message);
+    })
+    .catch((err) => {
+      console.log(err);
+      next();
+    });
 };
