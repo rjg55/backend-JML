16b9c54ce85c83f1a9999e635510efbc3c36fe7b
rjg55 <rikgraham55@gmail.com>
2022-09-07 09:46:28 +0100

    add get all groups endpoint with testing and some queries

--- backend/controllers/group-controller.js
+++ backend/controllers/group-controller.js
@@ -1,7 +1,10 @@
 const { fetchAllGroups } = require(`${__dirname}/../models/group-models.js`);
 
 exports.getAllGroups = (req, res, next) => {
-  fetchAllGroups()
-    .then(() => {})
+  const { sortby } = req.query;
+  fetchAllGroups(sortby)
+    .then((groups) => {
+      res.status(200).send({ groups: groups });
+    })
     .catch(next);
 };
--- backend/models/group-models.js
+++ backend/models/group-models.js
@@ -1,4 +1,20 @@
 const Groups = require(`${__dirname}/../schemas/group-schema.js`);
 const fs = require("fs/promises");
 
-exports.fetchAllGroups = async () => {};
+exports.fetchAllGroups = async (sortby = "members") => {
+  const validSortby = [
+    "title",
+    "category",
+    "description",
+    "members",
+    "admin",
+    "thanks",
+  ];
+
+  if (validSortby.includes(sortby)) {
+    const groupData = await Groups.find({}).sort(sortby);
+    return groupData;
+  } else {
+    return Promise.reject({ status: 400, msg: "Bad request" });
+  }
+};
