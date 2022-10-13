e067731325f3322e0d5b21789401129f0e442f1f
rjg55 <rikgraham55@gmail.com>
2022-10-13 14:51:39 +0100

    add console log to db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,6 +8,7 @@ dotenv.config({
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(process.env);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
